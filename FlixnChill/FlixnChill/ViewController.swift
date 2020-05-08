b9093f871104d25f1857c08fb95bc585cdadbaf9
Da'Shawn Larry <30280588+Dexsidius@users.noreply.github.com>
2020-05-08 09:04:58 -0400

    FlixnChill imported repository

---
+++ FlixnChill/FlixnChill/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FlixnChill
+//
+//  Created by Da'Shawn Larry on 5/7/20.
+//  Copyright © 2020 Da'Shawn Larry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+
+
+}
+
---
+++ FlixnChill/FlixnChillTests/FlixnChillTests.swift
@@ -0,0 +1,34 @@
+//
+//  FlixnChillTests.swift
+//  FlixnChillTests
+//
+//  Created by Da'Shawn Larry on 5/7/20.
+//  Copyright © 2020 Da'Shawn Larry. All rights reserved.
+//
+
+import XCTest
+@testable import FlixnChill
+
+class FlixnChillTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
